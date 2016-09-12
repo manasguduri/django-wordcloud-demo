07d5ea400904f8cbee21c3c62ad237ede44b0323
Paul Rudin <paul.rudin@grapeshot.com>
2016-09-12 18:55:13 +0100

    working using sdk

--- views.py
+++ views.py
@@ -4,7 +4,9 @@ from django.views.generic import TemplateView
 from django.views.generic.edit import FormView
 from django.core.urlresolvers import reverse_lazy
 
-from grapeshot_signal import SignalClient, rels, APIError, OverQuotaError
+
+from grapeshot_signal import \
+    SignalClient, rels, APIError, OverQuotaError
 
 import logging
 
@@ -15,7 +17,7 @@ from dev_portal.models.api_app import assure_account_app_list
 
 from .forms import WordcloudForm
 
-logger = logging.getLogger("wordcloud")
+logger = logging.getLogger("gs_dev_portal.wordcloud")
 
 
 class DemoListView(TemplateView):
@@ -29,7 +31,8 @@ def remap(vals, low=10, high=30):
     sizes.
 
     """
-
+    if (not vals):
+        return []
     min_val = min(vals)
     max_val = max(vals)
     scale_factor = (high - low) / (max_val - min_val)
@@ -46,27 +49,32 @@ class WordCloudView(FormView):
         api_account = assure_api_account_exists(self.request.user)
         app = assure_account_app_list(api_account)[0]
         key = app.apikey_set.first()
+        logger.debug("key is: {}".format(key.token()))
         return key.token()
 
     def form_valid(self, form, **kwargs):
 
         client = SignalClient(self._get_api_token())
+        client.base_url = settings.OPEN_API_URL
 
         url = form['target_url'].value()
 
         try:
-            page = client.get_page(url, [rels.keywords])
+            page = client.get_page(url, "keywords")
             if not page.is_error() and not page.is_queued():
                 keywords = page.get_embedded(rels.keywords)
             else:
                 keywords = []
         except (APIError, OverQuotaError, ConnectionError) as e:
-            logger.debug(e)
+            logger.debug("get_page exception: {}".format(e))
+            keywords = []
+
+        keywords = keywords['keywords']  # shouldn't get_embedded do this?
 
         vals = [item['score'] for item in keywords]
         new_vals = remap(vals)
         remapped_keywords = [list(v) for v in
-                             zip((item['name'] for item in vals),
+                             zip((item['name'] for item in keywords),
                                  new_vals)]
 
         context = self.get_context_data(**kwargs)
